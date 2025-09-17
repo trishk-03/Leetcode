62d8c257024dbcef906ec10985f9621183a27463
trishk-03 <trishak78807@gmail.com>
2025-09-17 11:18:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (74.17%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,29 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         if(s==t)
+//         return true;
+//         return false;
+//     }
+// };
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       vector<int> freq (256,0);
+       if(s.size()!= t.size()) return false;
+       for(int i =0; i<s.size();i++){
+        freq[s[i]-'a']++;
+        freq[t[i]-'a']--;
+       }
+       for(int i =0;i<freq.size();i++){
+        if(freq[i]!=0) return false;
+       }
+
+       return true;
+
+       
+    }
+};
