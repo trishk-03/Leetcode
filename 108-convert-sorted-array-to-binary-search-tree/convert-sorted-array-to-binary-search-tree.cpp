b1f667675b101dd4b094263fc832b98d8de12420
trishk-03 <trishak78807@gmail.com>
2025-10-10 09:34:43 +0530

    Time: 109 ms (5.15%) | Memory: 345.5 MB (5.63%) - LeetSync

---
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* bst(vector<int> v, int l , int r){
+        if(l>r)  return nullptr;
+        int mid = l +(r-l)/2;
+        TreeNode* root = new TreeNode(v[mid]);
+        root->left= bst(v,l,mid-1);
+        root->right = bst(v,mid+1,r);
+        return root;
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int l =0;
+        int r = nums.size()-1;
+        return bst(nums , l, r);
+
+        // first find the left and right index of an vector
+        // create a function and check if l> r then return null
+        // find the mid and create a root of treenode 
+        // recursively find the mid for left subtree and right subtree till it reaches the bse case 
+        // return the newly height balanced root subtree
+    }
+};
