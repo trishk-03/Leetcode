623765b5fb306b228b08c130426f033cd1ae8add
Trishak Jaiswal <trishak78807@gmail.com>
2025-10-14 15:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (90.54%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -13,15 +13,17 @@ class Solution {
 private:
     void sum_rtl(TreeNode* root, int &ans, int number){
         if(root ==  nullptr) return;
-        number = number*10 +root->val ; 
-        
 
+        // main logic
+        number = number*10 +root->val ; 
+
+        // if there is a leaf node then it means that after this no digit will be possible to add to that number then just add it to the digit and after this recuursion will handle for the all cases 
         if(root->left == nullptr && root ->right ==nullptr){
             ans += number;
             return; 
         }
-        sum_rtl(root->left,ans,number);
-        sum_rtl(root->right, ans,number);
+        sum_rtl(root->left,ans,number); // for left subtree
+        sum_rtl(root->right, ans,number); // for right subtree
     }
 public:
     int sumNumbers(TreeNode* root) {
