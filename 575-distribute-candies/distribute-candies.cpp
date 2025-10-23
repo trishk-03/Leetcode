e3690bc80e0b60361ba774a624b09b82018eb814
Trishak Jaiswal <trishak78807@gmail.com>
2025-10-23 15:34:13 +0530

    Time: 131 ms (28.94%) | Memory: 124.1 MB (12.62%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -1,16 +1,27 @@
-class Solution {
+class Approach_1 {
 public:
     int distributeCandies(vector<int>& candyType) {
-        int n = candyType.size()/2;
+        int n = candyType.size() / 2;
 
         set<int> s;
-        for(int i= 0;i<candyType.size();i++){
-            if(s.find(candyType[i])==s.end()){
+        for (int i = 0; i < candyType.size(); i++) {
+            if (s.find(candyType[i]) == s.end()) {
                 s.insert(candyType[i]);
             }
         }
 
-        if(n>=s.size()) return s.size();
-        else return n;
+        if (n >= s.size())
+            return s.size();
+        else
+            return n;
+    }
+};
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int n = candyType.size() / 2;
+        set<int> s(candyType.begin(),candyType.end()); 
+        return min((int)s.size(), n);
     }
 };
