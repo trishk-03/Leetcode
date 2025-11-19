cf226d26e46a2b6087aa8896b432c1557d3fad19
Trishak Jaiswal <trishak78807@gmail.com>
2025-11-19 18:00:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (85.64%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        vector<int>freq(26,0);
+
+        for(int i =0;i<s.size();i++){
+            if(freq[s[i]-'a'] !=0) return s[i];
+
+            freq[s[i]-'a']++;
+        }
+        return  s[0];
+    }
+};
