b56b8983685346944e044359262a582dac280a98
Trishak Jaiswal <trishak78807@gmail.com>
2026-01-30 12:05:35 +0530

    Time: 674 ms (12.84%) | Memory: 385.6 MB (6.58%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -15,6 +15,7 @@ private:
         }
     return false;
     }
+
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         unordered_map<int, vector<int>> graph;
@@ -25,7 +26,6 @@ public:
             graph[v].push_back(u);
         }
         unordered_set<int> visited;
-        return dfs(source, destination,graph , visited);
-        
+        return dfs(source, destination,graph , visited);        
     }
 };
