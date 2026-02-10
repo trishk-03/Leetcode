d2ba76d6c37f787547f634943bedfc563319226d
Trishak Jaiswal <trishak78807@gmail.com>
2026-02-10 18:55:38 +0530

    Time: 5 ms (66.44%) | Memory: 128.1 MB (99.79%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>a;
-        vector<int>b;
+        // optimal solution
+        vector<int> ans(nums.size(),0);
+        int p =0, n=1;
+
         for(int i =0;i<nums.size();i++){
             if(nums[i]>0){
-                a.push_back(nums[i]);
+                ans[p]= nums[i];
+                p = p+2;
             }
             else{
-                b.push_back(nums[i]);
+                ans[n]= nums[i];
+                n= n+2;
             }
-        }
-
-        vector<int> ans;
-        for(int i =0;i<a.size();i++){
-            ans.push_back(a[i]);
-            ans.push_back(b[i]);
         }
         return ans;
     }
 };
+
+        // brute force 
+        // vector<int>a;
+        // vector<int>b;
+        // for(int i =0;i<nums.size();i++){
+        //     if(nums[i]>0){
+        //         a.push_back(nums[i]);
+        //     }
+        //     else{
+        //         b.push_back(nums[i]);
+        //     }
+        // }
+
+        // vector<int> ans;
+        // for(int i =0;i<a.size();i++){
+        //     ans.push_back(a[i]);
+        //     ans.push_back(b[i]);
+        // }
+        // return ans;
