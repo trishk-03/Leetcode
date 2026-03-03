c390d1557b0ffa0fd49515a1ceae830649e142ae
Trishak Jaiswal <trishak78807@gmail.com>
2026-03-03 16:05:51 +0530

    Time: 3 ms (35.69%) | Memory: 11 MB (35.51%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        string word="";
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] != ' ') 
+            word.push_back(s[i]);
+
+            else if(!word.empty()){
+                words.push_back(word);
+                word ="";
+            }
+        }
+        if (!word.empty()) {
+            words.push_back(word);
+        }
+        reverse(words.begin(), words.end());
+
+        string result = "";
+        for (int i = 0; i < words.size(); i++) {
+            result += words[i];
+            if (i < words.size() - 1) {
+                result += " ";
+            }
+        }
+        
+        return result;
+    }
+};
