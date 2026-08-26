2a9898e85b18ab66e74436ef7e74dba053b78f63
Trishak Jaiswal <trishak78807@gmail.com>
2026-08-27 01:46:49 +0530

    Time: 34 ms (98.90%) | Memory: 56.7 MB (39.84%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let val = init;
+    return {
+            increment(){
+             return val +=1;
+            },
+            decrement(){
+            return val -=1;
+            },
+        reset(){
+            return val = init;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
